0943a1f4d7e92e2393a41b04278e3f49eb6b6efb
Daniel Arcos <91172879+Daniel-Arcos@users.noreply.github.com>
2024-04-29 00:42:28 -0600

    Upload files

---
+++ server/AudioServicer.cs
@@ -0,0 +1,28 @@
+using Google.Protobuf;
+using Grpc.Core;
+
+public class AudioServicer : AudioService.AudioServiceBase
+{
+
+    private const int CHUNK_SIZE = 1024;
+
+    public override async Task downloadAudio(DownloadFileRequest request, IServerStreamWriter<DataChunkResponse> responseStream, ServerCallContext context)
+    {
+        var buffer = new byte[CHUNK_SIZE];
+        await using var fileStream = File.OpenRead($"resources\\{request.Nombre}");
+        var numBytesRead = 0;
+
+        Console.WriteLine($"\n\nEnviando el archivo: {request.Nombre}");
+        try 
+        {
+            while ((numBytesRead = await fileStream.ReadAsync(buffer)) > 0)
+            {
+                await responseStream.WriteAsync(new DataChunkResponse {
+                    Data = UnsafeByteOperations.UnsafeWrap(buffer.AsMemory(0, numBytesRead))
+                });
+                Console.Write(".");
+            }
+        }
+        catch {}
+    }
+}
